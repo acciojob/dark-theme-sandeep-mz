ca370f1acb0dcc5bf5bcc8b91739b131a680eaff
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-18 18:21:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here
+// JavaScript to toggle the dark theme
 
+// Function to toggle the theme when the button is clicked
+function swapTheme() {
+  const mainElement = document.querySelector('#app');
+  const buttonElement = document.querySelector('#swap');
+
+  if (mainElement.classList.contains('night')) {
+    mainElement.classList.remove('night');
+    buttonElement.classList.remove('button_night');
+    buttonElement.textContent = 'Dark';
+  } else {
+    mainElement.classList.add('night');
+    buttonElement.classList.add('button_night');
+    buttonElement.textContent = 'Light';
+  }
+}
+
+// Add a click event listener to the button
+const buttonElement = document.querySelector('#swap');
+buttonElement.addEventListener('click', swapTheme);
